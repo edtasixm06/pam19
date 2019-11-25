87423f74f6edd7b37a979fce4d005298b7fd83a8
Eduard Canet <ecanet@localhost.localdomain>
2019-11-25 10:44:51 +0100

    hostpam19:scratch

--- hostpam19:scratch/install.sh
+++ hostpam19:scratch/install.sh
@@ -5,7 +5,5 @@ useradd local3
 echo "local1" | passwd --stdin local1
 echo "local2" | passwd --stdin local2
 echo "local3" | passwd --stdin local3
-#cp /opt/docker/nslcd.conf /etc/nslcd.conf
-#cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 bash /opt/docker/auth.sh
 
