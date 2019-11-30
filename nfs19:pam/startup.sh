6aa5160b81aea50abf71b020a9d568dd48429ee4
jclotsmxm04 <jclotsmxm04@gmail.com>
2019-11-30 20:36:24 +0100

    pam

--- nfs19:pam/startup.sh
+++ nfs19:pam/startup.sh
@@ -11,7 +11,7 @@
 
 # Configuració samba
 /usr/sbin/smbd && echo "smb Ok"
-/usr/sbin/nmbd && echo "nmb  Ok"
+# /usr/sbin/nmbd && echo "nmb  Ok"
 
 # -------------------------------------------------------------------
 # Creació dels comptes samba i directoris dels usuaris ldap ---------
@@ -19,12 +19,14 @@
 bash /opt/docker/ldapusers.sh
 # ===================================================================
 
-
 # -------------------------------------------------------------------
 # Activació del servei nfs
 /usr/sbin/rpcbind && echo "rpcbind Ok"
 /usr/sbin/rpc.statd && echo "rpc.stad Ok"
 /usr/sbin/rpc.nfsd && echo "rpc.nfsd Ok"
 /usr/sbin/rpc.mountd && echo "rpc.mountd Ok"
-/usr/sbin/exportfs -av
+/usr/sbin/exportfs -av 
 # -------------------------------------------------------------------
+
+/usr/sbin/nmbd -F
+
--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ docker volume create homes
 
 docker run --rm --name ldapserver -h ldapserver --net ldapnet -d edtasixm06/ldapserver19:latest
 
-docker run --rm --name samba -h samba --net ldapnet -p 139:139 -p 445:445 -v homes:/tmp/home   --privileged   -it edtasixm06/samba19:pam
-docker run --rm --name nfs -h nfs --net ldapnet -v homes:/tmp/home   --privileged   -it edtasixm06/nfs19:pam
+docker run --rm --name samba -h samba --net ldapnet -p 139:139 -p 445:445 -v homes:/tmp/home   --privileged   -d edtasixm06/samba19:pam
+docker run --rm --name nfs -h nfs --net ldapnet -v homes:/tmp/home   --privileged   -d edtasixm06/nfs19:pam
 
 docker run --rm --name pam -h pam --net ldapnet --privileged -it edtasixm06/hostpam19:samba /bin/bash
 
